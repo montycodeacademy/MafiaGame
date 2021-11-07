8492c1233ba48295eed107d8fb38170da6161c40
Vasu Mahadasu <vasu.mahadasu@gmail.com>
2021-11-07 16:31:14 -0600

    Added Profile Save and Retrieve

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ from kivy.core.window import Window
 from kivy.utils import platform
 from kivy.app import App
 
+
 from kivymd.app import MDApp
 from kivymd.uix.navigationdrawer import MDNavigationDrawer
+from kivymd.toast import toast
+
 
 class PlayerProfile(MDNavigationDrawer):
     def __init__(self, **kwargs):
@@ -14,38 +17,47 @@ class PlayerProfile(MDNavigationDrawer):
         super().__init__(**kwargs)
 
     def save_profile(self):
+        toast("Profile Saved", 2.5)
+        profile_string = self.profile_email + " " + self.profile_nickname
         with open(self.profile_data_file, "w") as outfile:
-            outfile.write(self.profile_email)
-            outfile.write(self.profile_nickname)
+            outfile.write(profile_string)
 
     def load_profile(self):
         with open(self.profile_data_file, "r") as infile:
-            self.profile_email = infile.read()
-            self.profile_nickname = infile.read()
+            profile_string = infile.read()
+            items = profile_string.split()
+            self.profile_email = items[0]
+            self.profile_nickname = items[1]
             print(self.profile_email)
+            print(self.profile_nickname)
 
     def update_status(self, *_):
         Save_Profile = False
         if self.open_progress == 0 and self.status == "closing_with_animation":
             if len(self.email.text):
-                self.profile_email = self.email.text
-                Save_Profile = True
+                if (self.profile_email != self.email.text):
+                    self.profile_email = self.email.text
+                    Save_Profile = True
             else:
                 print("Need a valid email")
 
-
             if len(self.nickname.text):
-                self.profile_nickname = self.nickname.text
-                Save_Profile = True
+                if (self.profile_nickname != self.nickname.text):
+                    self.profile_nickname = self.nickname.text
+                    Save_Profile = True
             else:
                 print("Need a valid nickname")
 
-            #print(self.root.ids.avatar.source)
+        elif self.open_progress == 1 and self.status =="opening_with_animation":
+            self.email.text = self.profile_email
+            self.nickname.text = self.profile_nickname
 
         super().update_status()
 
         if Save_Profile:
             self.save_profile()
+
+
 
 class MafiaGameApp(MDApp):
 
